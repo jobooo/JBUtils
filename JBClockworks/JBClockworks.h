6813e25edb314f6dace82d9f3f50d2405b206bc6
Jocelyn Bouchard <jboreillegauche@gmail.com>
2021-05-30 02:08:06 -0400

    Update JBClockworks.h

--- JBClockworks/JBClockworks.h
+++ JBClockworks/JBClockworks.h
@@ -4,56 +4,56 @@
 
 #ifndef JBClockWorks_VERSION
 
-#define JBClockWorks_VERSION "2021-05-28"
+#define JBClockWorks_VERSION "2021-05-29"
 
 class JBCountDown {
 private:
     uint32_t start_millis=0;
-    uint32_t now_millis=0;
     uint32_t NbCountDownMillis;
 public:
-    uint32_t initNbSec, MillisLeft, SecLeft, MinLeft, HoursLeft;
+    uint32_t MillisLeft, SecLeft, MinLeft, HoursLeft;
     bool Started=false;
 
     JBCountDown(uint32_t nbSeconds){
-        initNbSec=nbSeconds;
         NbCountDownMillis=nbSeconds*1000;
-        Update();
+        Reset();
     }
 
     void Reset(){
         Stop();
         start_millis=0;
-        now_millis=0;
-        Update();
+        MillisLeft=NbCountDownMillis;
+        SecLeft=MillisLeft/1000;
+        MinLeft=SecLeft/60;
+        HoursLeft=MinLeft/60;
     }
 
     bool Start(){
         Started=true;
         start_millis=millis();
-        MillisLeft=;
+        MillisLeft=NbCountDownMillis;
     }
-    void Stop(){Started=false;}
+
+    void Stop(){
+        Started=false;
+    }
 
     // Update() returns true if there is time left (it returns Started bool value).
     bool Update(){
-        
-        MinLeft=SecLeft/60;
-        HoursLeft=MinLeft/60;
         if(Started){
-            now_millis=millis();
-            SecLeft=(now_millis-start_millis)/1000;
-            if(now_millis-start_millis>=)
-
-
+            MillisLeft=NbCountDownMillis - (millis()-start_millis);
+            if(MillisLeft <= 0){
+                Started=false;
+                MillisLeft=SecLeft=MinLeft=HoursLeft=0;
+                Started=false;
+            }else{
+                SecLeft=MillisLeft/1000;
+                MinLeft=SecLeft/60;
+                HoursLeft=MinLeft/60;
+            }
         }
         return Started;
     }
-
-
 };
-
-
-
 
 #endif
